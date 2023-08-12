0f3e598fd9d811ff9e08143c49a7f332b10028c7
isaacdannykachi <isaacdannykachi@gmail.com>
2023-08-11 18:02:23 -0700

    C program that prints lowercase alphabets with a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - origin point
+ * Description: Print lowercase alphabets
+ * Return: (0) (Success)
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar return (0);
+}

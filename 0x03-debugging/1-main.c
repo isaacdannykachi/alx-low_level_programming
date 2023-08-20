07abe28db4bb7164c9c718052f8522c185212df7
isaacdannykachi <isaacdannykachi@gmail.com>
2023-08-20 12:27:15 -0700

    debugging 1-main.c to prevent a continous loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -13,7 +14,7 @@ int main(void)
 	i = 0;
 
 	/**
-	* while (i < 10)
+	 * while (i < 10)
 	* {
 	*	putchar(i);
 	* }

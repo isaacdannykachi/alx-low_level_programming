1f405405fad33c1f5d81502b17efd1669c606f0a
isaacdannykachi <isaacdannykachi@gmail.com>
2023-09-04 17:55:20 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,43 @@
-#include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string
- *
- * Return: char pointer
- */
+* str_concat - Concatenate two strings
+* @s1: A string to be concatenated
+* @s2: A string to be concatenated
+* Return: Pointer to newly allocated memory
+* which is equal to @s1 + @s2 (SUCCESS) OR
+* NULL (FAILURE)
+*/
+
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int l1 = 0, l2 = 0, i, j;
-	char *cnctd;
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
+	char *alloc_mem;
+	unsigned int i, j;
 
 	if (s1 == NULL)
-	{
-		s1 = " ";
-	}
+		s1 = "";
+
 	if (s2 == NULL)
-	{
-		s2 = " ";
-	}
+		s2 = "";
 
-	 l1 = 0, l2 = 0;
+	while (*(s1 + s1_length) != '\0')
+		s1_length++;
 
-	while (s1[l1] != '\0')
-	{
-		l1++;
-	}
-	while (s2[l2] != '\0')
-	{
-		l2++;
-	}
+	while (*(s2 + s2_length) != '\0')
+		s2_length++;
 
-	cnctd = (char *)malloc((l1 + l2 + 1) * sizeof(char));
+	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
 
-	if (cnctd == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < l1; i++)
-	{
-		cnctd[i] = s1[i];
-	}
-	for (j = 0; j < l2; j++)
-	{
-		cnctd[i++] = s2[j];
-	}
+	if (alloc_mem == NULL)
+		return ('\0');
 
-	cnctd[i] = '\0';
-	return (cnctd);
+	for (i = 0; i < s1_length; i++)
+		alloc_mem[i] = *(s1 + i);
+
+	for (j = 0; j < s2_length; j++, i++)
+		alloc_mem[i] = *(s2 + j);
+
+	return (alloc_mem);
 }

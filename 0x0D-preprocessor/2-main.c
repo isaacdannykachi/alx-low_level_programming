89fdeb59d0e50027b088bca8be9ffc2df6029082
isaacdannykachi <isaacdannykachi@gmail.com>
2023-09-11 18:57:38 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the file
+ *
+ * Return: 0 if succes 1 otherwise
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+preprocessor projects and task
